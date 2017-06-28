22a84663360080b69f490c29cbb35299d1d7165e
jmestrez0r <mestre.jorge@gmail.com>
2017-06-28 23:17:36 +0100

    refreshing players details

--- js/controllers/teams-index-controller.js
+++ js/controllers/teams-index-controller.js
@@ -1,8 +1,36 @@
-angular.module("Elifoot").controller('TeamPlayersController', function($scope, $timeout, TeamPlayers) {
+angular.module("Elifoot").controller('TeamPlayersController', function($scope, $timeout, TeamPlayers, ngDialog) {
+
+  $scope.selectedPlayer = '';
 
   TeamPlayers.all().success(function(data) {
-      $scope.teamPlayers = data;
-      console.log(data);
+    $scope.goalKeepers = [];
+    $scope.defenses = [];
+    $scope.middles = [];
+    $scope.strikers = [];
+    var keepersIndex = 0;
+    var middlesIndex = 0;
+    var defensesIndex = 0;
+    var strikersIndex = 0;
+
+    for(var i = 0; i < data.players.length; i++) {
+      var a = data.players[i];
+
+      if(a.position.includes('Keeper')) {
+        $scope.goalKeepers[keepersIndex] = a;
+        keepersIndex++;
+      } else if ((a.position.includes('Centre') && !a.position.includes('Forward'))
+        || a.position.includes('Wing')) {
+        $scope.middles[middlesIndex] = a;
+        middlesIndex++;
+      } else if (a.position.includes('Back') || a.position.includes('Defensive')) {
+        $scope.defenses[defensesIndex] = a;
+        defensesIndex++;
+      } else if (a.position.includes('Forward') || a.position.includes('Striker')) {
+        $scope.strikers[strikersIndex] = a;
+        strikersIndex++;
+      }
+    }
+    console.log(data);
   });
 
   TeamPlayers.effectiveTeam().success(function (data) {
@@ -33,4 +61,35 @@ angular.module("Elifoot").controller('TeamPlayersController', function($scope, $
       }
     }
   };
+
+  $scope.openDeletePlayerDialog = function(player) {
+    $scope.selectedPlayer = player;
+
+    ngDialog.open({
+      template: 'deletePlayer.html',
+      className: 'ngdialog-theme-default',
+      scope: $scope,
+      showClose: false,
+      height: 250,
+      weight: 600
+    });
+  };
+
+  $scope.openPlayerInformationDialog = function(player) {
+    $scope.selectedPlayer = player;
+
+    ngDialog.open({
+      template: 'playerInformation.html',
+      className: 'ngdialog-theme-default',
+      scope: $scope,
+      showClose: false,
+      height: 250,
+      weight: 600
+    });
+  };
+
+  $scope.deletePlayer = function() {
+     //remove the player
+  };
+
 });
